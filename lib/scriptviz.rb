92205400b59f744fa32fe9fe6e3cb24193a77ef0
meleneth <meleneth@rizzo>
2014-02-19 22:18:55 -0800

    Add scriptviz skeleton files

---
+++ lib/scriptviz.rb
@@ -0,0 +1,3 @@
+class Scriptviz
+
+end
---
+++ spec/scriptviz_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+require 'scriptviz'
+
+describe "Scriptviz" do
+  describe "#new" do
+    it "Doesnt take arguments" do
+      sniffer = Scriptviz.new
+    end
+  end
+end
